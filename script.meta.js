947a801b5f85524791d3921a9540ead0bf9b9973
rek655689 <e.gladneva1357@gmail.com>
2026-03-24 13:09:59 +0300

    разделение на meta и user

---
+++ script.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Проверка должностей
+// @author       rek655869
+// @license      MIT
+// @version      2.0.2
+// @match        https://catwar.su/blog*
+// @match        https://catwar.net/blog*
+// @match        https://catwar.su/my_clan/link
+// @match        https://catwar.net/my_clan/link
+// @updateURL    https://raw.githubusercontent.com/rek655869/checking_positions/refs/heads/master/script.meta.js
+// @downloadURL  https://raw.githubusercontent.com/rek655869/checking_positions/refs/heads/master/script.user.js
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js
+// ==/UserScript==
--- script.user.js
+++ script.user.js
@@ -7,6 +7,8 @@
 // @match        https://catwar.net/blog*
 // @match        https://catwar.su/my_clan/link
 // @match        https://catwar.net/my_clan/link
+// @updateURL    https://raw.githubusercontent.com/rek655869/checking_positions/refs/heads/master/script.meta.js
+// @downloadURL  https://raw.githubusercontent.com/rek655869/checking_positions/refs/heads/master/script.user.js
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js
 // ==/UserScript==
 (function () {
